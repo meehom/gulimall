fb692cbb4f96b4dcf45575f0db47a7187887b345
meehom <meehomliao@163.com>
2022-03-04 19:40:44 +0800

    初始化项目

---
+++ gulimall-coupen/src/main/java/com/atguigu/coupen/GulimallCoupenApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.coupen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallCoupenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GulimallCoupenApplication.class, args);
+	}
+
+}
---
+++ gulimall-coupen/src/test/java/com/atguigu/coupen/GulimallCoupenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.coupen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCoupenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
